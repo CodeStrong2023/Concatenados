f284fdc5b53929ef603cc2f45698d5812ea757ea
Franco pieroni <francopiero594@gmial.com>
2023-09-07 02:42:14 -0300

    Agregando clase 3 Laboratorio y JavaScript a mi rama

---
+++ Franco Pieroni/Javascript/Leccion02/02-01-TiposDatos.js
@@ -0,0 +1,14 @@
+var nombre = 'Ariel';
+console.log(nombre); //Tipo string
+
+var numero = 3000;
+console.log(numero); //Tipo numerico
+
+var objeto = {
+    nombre : 'Ariel',
+    apellido : 'Betancud',
+    telefono : '261456879' 
+}
+
+console.log(objeto);
+
