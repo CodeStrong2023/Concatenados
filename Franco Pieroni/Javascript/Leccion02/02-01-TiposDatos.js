fc3c36545a508dcd5bd8b198ab15f46e52f2b06b
Franco pieroni <francopiero594@gmial.com>
2023-09-14 08:17:53 -0300

    Agregando clase 4 JavaScript

--- Franco Pieroni/Javascript/Leccion02/02-01-TiposDatos.js
+++ Franco Pieroni/Javascript/Leccion02/02-01-TiposDatos.js
@@ -1,6 +1,9 @@
 var nombre = 'Ariel';
-console.log(nombre); //Tipo string
-
+console.log(typeof nombre); //Tipo string
+nombre = 7;
+console.log(typeof nombre);
+nombre = 12.3;
+console.log(typeof nombre);
 var numero = 3000;
 console.log(numero); //Tipo numerico
 
@@ -13,3 +16,24 @@ var objeto = {
 
 console.log(objeto);
 
+//Tipo de dato boolean
+var bandera = true
+console.log(bandera);
+
+//Tipo de dato función
+function miFuncion(){}
+console.log(typeof miFuncion)
+
+//Tipo de dato symbol
+var simbolo = Symbol('Mi simbolo');
+console.log(typeof simbolo);
+
+//Tipo de dato clase
+class Persona{
+    contructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+console.log(typeof Persona);
