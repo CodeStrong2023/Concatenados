31b665b0efb66e1a6ea99f52a8d1fef42363ae38
Franco pieroni <francopiero594@gmial.com>
2023-09-15 00:29:43 -0300

    Agregando ejercicios clase 5 Java

--- Franco Pieroni/Java/Leccion4/Aritmética/src/Operaciones/Aritmética.java
+++ Franco Pieroni/Java/Leccion4/Aritmética/src/Operaciones/Aritmética.java
@@ -12,5 +12,17 @@ public class Aritmética { // La Clase siempre usa Pacal case, es decir comienza
         int resultado = a + b;
         System.out.println("La suma de los dígitos es: " + resultado);
     }
+    
+    public int sumarConRetorno() {
+        int resultado = a + b;
+        return resultado;
+    }
+    
+    public int sumarConArgumentos(int a, int b) {
+        this.a = a; // El argumento a se asigna al atributo this.a
+        this.b = b; 
+        //return a + b;
+        return this.sumarConRetorno();
+    }
 }
  
---
+++ Franco Pieroni/Java/Leccion4/Aritmética/src/Operaciones/PruebaAritmetica.java
@@ -0,0 +1,21 @@
+
+package Operaciones;
+
+
+public class PruebaAritmetica {
+    public static void main(String[] args) {
+        Aritmética aritmetica1 = new Aritmética();
+        aritmetica1.a = 3;
+        aritmetica1.b = 7;
+        aritmetica1.sumarNumeros();
+        
+        int resultado = aritmetica1.sumarConRetorno();
+        System.out.println("resultado = " + resultado );
+        
+         resultado = aritmetica1.sumarConArgumentos(12, 26);
+        System.out.println("Resultado usando argumentos: " + resultado);
+        
+        
+        
+    }
+}
