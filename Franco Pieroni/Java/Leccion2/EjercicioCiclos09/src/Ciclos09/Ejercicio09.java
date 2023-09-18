09a24c2e6ac727150ab5daf8aea87e5a3f65518c
Franco pieroni <francopiero594@gmial.com>
2023-09-18 19:21:15 -0300

    Agregando ejercicio09 Java

--- Franco Pieroni/Java/Leccion2/EjercicioCiclos09/src/Ciclos09/Ejercicio09.java
+++ Franco Pieroni/Java/Leccion2/EjercicioCiclos09/src/Ciclos09/Ejercicio09.java
@@ -9,9 +9,7 @@ public class Ejercicio09 {
     public static void main(String[] args) {
         
         int dia = Integer.parseInt(JOptionPane.showInputDialog("Digite el día: "));
-        System.out.println("Digite mes: ");
         int mes = Integer.parseInt(JOptionPane.showInputDialog("Digite el mes: "));
-        System.out.println("Digite año: ");
         int año = Integer.parseInt(JOptionPane.showInputDialog("Digite el año: "));
         if ((dia != 0)&&(dia <= 30)) {
             if ((mes != 0)&&(mes <= 12)){
