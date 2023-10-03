c5e0de277ad94509c16eb51f5f2e440ee19f8340
Franco pieroni <francopiero594@gmail.com>
2023-10-02 22:47:28 -0300

    Agregando ejercicios JS

---
+++ Franco Pieroni/Javascript/Leccion02/02-01-ConcatenacionDeCadenas.js
@@ -0,0 +1,22 @@
+var nombre = "Jose";
+var apellido = " Montes";
+var nombreCompleto = nombre +" "+ apellido;
+console.log(nombreCompleto)
+var nombreCompleto2 =  "Ariel" +" "+"Betancud";
+console.log(nombreCompleto2);
+var juntos = nombre + 219; // Lee de izquierda a derecha como una cadena
+console.log(juntos);
+juntos = nombre + 78 + 17;
+console.log(juntos);
+juntos = 78 + 17 + nombre;
+console.log(juntos);
+var juntos = nombre + 219; // Lee de izquierda a derecha coo string
+console.log(juntos);
+juntos = nombre + 78 + 17; // Aquí se puede diferencia a través d elos paréntesis
+console.log(juntos);
+juntos = 78 + 17 + nombre;
+console.log(juntos)
+
+nombre += apellido; 
+console.log(nombre);
+
