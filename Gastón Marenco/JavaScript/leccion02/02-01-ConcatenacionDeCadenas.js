65bad23367b0d3bfec4db84dc681fa4ae7e2bf64
GastonMarenco <101683116+GastonMarenco@users.noreply.github.com>
2023-10-20 01:56:41 -0300

    Diferencia entre var, let y const clase 8 Js
    Clase 8 de JavaScript

--- Gastón Marenco/JavaScript/leccion02/02-01-ConcatenacionDeCadenas.js
+++ Gastón Marenco/JavaScript/leccion02/02-01-ConcatenacionDeCadenas.js
@@ -34,3 +34,54 @@ let rompiendo = 'rompe'; // No utilizar palabras reservadas para variables
 
 console.log(_1num);
 console.log(rompiendo);
+
+// Almpliando el uso de var, let y const
+/*
+Con var puedes reasignar en cualquier momento
+este forma parte del ambito global
+Un error es que se sobreescriba
+*/
+
+var nombre = 'Gastón';
+nombre = 'Lionel';
+console.log(nombre);
+
+function saludar(){
+    var nombre3 = 'Cristiano';
+    console.log(nombre3);
+}
+//console.log(nombre3); // Aquí no lee el dato en la función
+
+if(true){
+    var edad = 34;
+    console.log(edad);
+}
+console.log(edad); // En la función funciono correctamente, en la estructura if fallo
+
+/*
+let: esta puede ser reasignada en cualquier momento
+la diferencia es que su ambito es de bloque
+solo disponible dentro de un bloque de llaves
+o dentro de una función
+*/
+
+function saludar2(){
+    let nombre2= 'Gastón';
+    console.log(nombre2);
+}
+//console.log(nombre2)
+
+if(true){
+    let edad2 = 33;
+    console.log(edad2);
+}
+//console.log(edad2);
+
+/*
+const se utiliza para valores constantes que no pueden ser reasignados
+*/
+
+const fechaNacimiento = 2006;
+console.log(fechaNacimiento);
+//fechaNacimiento = 2003;
+//console.log(fechaNacimiento); // solo se ejecuta el console anterior
