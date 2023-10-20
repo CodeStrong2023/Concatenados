32d4b4a09476a98818cfacc8968a711ebdd73049
GastonMarenco <101683116+GastonMarenco@users.noreply.github.com>
2023-10-20 00:55:22 -0300

    Agregando ejercicios leccion03 Js

---
+++ Gastón Marenco/JavaScript/leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -0,0 +1,17 @@
+// Ejercicio para encontar números pares e impares
+let parImpar = 2;
+if(parImpar % 2 == 0){
+    console.log("Es un número PAR");
+}
+else{
+    console.log("Es un número impar");
+}
+
+// Ejercicio: es mayor o menor de edad
+let edad = 20, adulto = 18;
+if(edad >= adulto){
+    console.log("Usted es una persona adulta")
+}
+else{
+    console.log("Usted es una persona menor de edad")
+}
