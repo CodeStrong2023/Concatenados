4238b6e9648caea7a9c8bb3b7d81d1cc9331732c
GastonMarenco <101683116+GastonMarenco@users.noreply.github.com>
2023-10-24 00:08:06 -0300

    Clases en java con break, continue y labels

--- Gastón Marenco/Java/Leccion2/CicloWhile/src/main/java/CicloWhile/EjercicioWhile01.java
+++ Gastón Marenco/Java/Leccion2/CicloWhile/src/main/java/CicloWhile/EjercicioWhile01.java
@@ -7,23 +7,35 @@ package CicloWhile;
 public class EjercicioWhile01 {
 
     public static void main(String[] args) {
-        
+
         System.out.println("\nCiclo While:");
         var conteo = 0;  // Inferencia de tipos
         while (conteo < 3) {
             System.out.println("conteo = " + conteo);
             conteo++;  // Vamos aumentando en 1 la variable
         }
-        
+
         System.out.println("\nCiclo Do While:");
         var contador = 0;
-        do{
+        do {
             System.out.println("contador = " + contador);
             contador++;
-        }while(contador < 7);
-        
+        } while (contador < 7);
+
+        //Uso de las palabras break y continue junto a las etiquetas (labels)
         System.out.println("\nCiclo For:");
-        for(var contando = 0; contando < 7; contando++){
+        for (var contando = 0; contando < 7; contando++) {
+            if (contando % 2 == 0) {
+                System.out.println("contando = " + contando);
+                break;
+            }
+        }
+        
+        inicio:
+        for (var contando = 0; contando < 7; contando++) {
+            if (contando % 2 != 0) {
+                continue inicio; //Vamos a la siguiente iteración
+            }
             System.out.println("contando = " + contando);
         }
     }
