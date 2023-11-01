87e8e135b70bcabe1189f0c5f55a390cd564748d
Sofia <sofyduple58@gmail.com>
2023-10-31 21:17:52 -0300

    9.2 Herencia parte 2

---
+++ Java/Leccion6/Herencia/src/domain/Empleado.java
@@ -0,0 +1,34 @@
+
+package domain;
+
+
+public class Empleado extends Persona{
+    private int idEmpleado;
+    private double sueldo;
+    private static int contadorEmpleados; //Es para incrementar
+    
+    //Constructor
+    public Empleado(String nombre, double sueldo) {
+        super(nombre);
+        this.idEmpleado = ++Empleado.contadorEmpleados;
+        this.sueldo = sueldo;
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+    
+    
+}
---
+++ Java/Leccion6/Herencia/src/test/TestHerencia.java
@@ -0,0 +1,13 @@
+
+package test;
+
+import domain.Empleado;
+
+public class TestHerencia {
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado();
+        System.out.println("empleado1 = " + empleado1);
+    }
+    
+    
+}
