2f3df7acece76929e4f73de1d815cb32ed332518
Franco Pieroni <francopiero594@gmail.com>
2023-11-02 19:53:49 -0300

    Ejercicios JS
    Ejercicios JS con condicionales, operador ternario y switch

--- Franco Pieroni/Javascript/Leccion03/03-01-OperadoresJavaScript-UJS.js
+++ Franco Pieroni/Javascript/Leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -25,3 +25,37 @@ if (dentroRango >= valMin && dentroRango <= valMax){
 } else {
     console.log("Esta fuera del rango")
 }
+
+// Ejercicio: si el padre puede asistir al juego de su hijo
+let vacaciones = false, diaDescanso = false;
+if (vacaciones || diaDescanso){
+    console.log("El padre puede asistir al juego de su hijo")
+}else {
+    console.log("El padre no puede asistir al juego de su hijo")
+}
+
+// Operador ternario
+let resultado2 = 3 > 3 ? "Verdadero" : "Falso" 
+console.log(resultado2)
+let numero = 9;
+resultado2 = numero % 2 == 0 ? "Es un número PAR" : "Es un número IMPAR";
+console.log(resultado2)
+
+// Convertir String a Number
+let miNumero = "10"; // Es una cadena
+console.log(typeof miNumero);
+let edad2 = Number(miNumero) // Esta es una función
+console.log(typeof edad2)
+if(isNaN(edad2)){  // Not a Number, no es un número
+    console.log("Esta variable no contiene solo números")
+}else {
+    if(edad2 >= 18){
+        console.log("Puede votar");
+    }else {
+        console.log("Muy joven para votar");
+    }
+}
+
+
+let resultado3 = edad >= 18 ? "Puede votar " : "Muy joven para votar";
+console.log(resultado3);
