216838f789c3419468d618a7a365377d7985d3df
Franco Pieroni <francopiero594@gmail.com>
2023-11-02 02:40:39 -0300

    Operadores en JS y ejercicios
    Ejercicios impar, rango y es mayor de edad

--- Franco Pieroni/Javascript/Leccion02/02-01-ConcatenacionDeCadenas.js
+++ Franco Pieroni/Javascript/Leccion02/02-01-ConcatenacionDeCadenas.js
@@ -10,13 +10,35 @@ juntos = nombre + 78 + 17;
 console.log(juntos);
 juntos = 78 + 17 + nombre;
 console.log(juntos);
-var juntos = nombre + 219; // Lee de izquierda a derecha coo string
+var juntos = nombre + 219; // Lee de izquierda a derecha como string
 console.log(juntos);
 juntos = nombre + 78 + 17; // Aquí se puede diferencia a través d elos paréntesis
 console.log(juntos);
 juntos = 78 + 17 + nombre;
 console.log(juntos)
 
-nombre += apellido; 
+nombre += apellido;  // Tercera Concatenación usando el operador simplificado
 console.log(nombre);
+
+// Hoy ya no se usa var, se utiliza let y const
+let nombre2 = "Pedro"
+console.log(nombre2);
+
+const apellido2 = "Lepes";
+// apellido2 = "Peres"; una constante no puede ser modificada
+console.log(apellido2)
+
+let x, y; // Se pueden crear varias variables dentro de una misma linea
+x = 17, y = 21; // Se pueden hacer asignación de varias variables dentro de una misma linea
+let z = x + y; // Se asigna el valor de la operación
+console.log(z);
+
+let $1num =  31; // No se utilizan números para iniciar le nombre de una variable
+let rompiendo = "rompe"; // No se utilizan palabras reservadas para variables
+
+
+
+
+
+
 
---
+++ Franco Pieroni/Javascript/Leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -0,0 +1,27 @@
+// Ejercicio para encontrar números pares
+
+let parInpar = 2;
+if(parInpar % 2 == 0){
+    console.log("Es un número par")
+} else {
+    console.log("Es un número inpar")
+}
+
+// Ejercicio es mayor de edad
+
+let edad = 2;
+if(edad > 18){
+    console.log("Es mayor de edad")
+} else {
+    console.log("Es menor de edad")
+}
+
+
+// Ejercicio dentro de un rango
+let dentroRango = 0; // Aquí vamos a ir cambiando el valor
+let valMin = 0, valMax = 10;
+if (dentroRango >= valMin && dentroRango <= valMax){
+    console.log("Está dentro del rango")
+} else {
+    console.log("Esta fuera del rango")
+}
