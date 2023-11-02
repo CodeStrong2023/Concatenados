da042f18c174c7708f7273cec61158f3e2fe4e5f
GastonMarenco <101683116+GastonMarenco@users.noreply.github.com>
2023-11-02 06:45:14 -0300

    Clase 9 de Java con herencia

---
+++ Gastón Marenco/Java/Leccion6/Herencia/src/main/java/test/TestHerencia.java
@@ -0,0 +1,21 @@
+package test;
+
+import domain.Cliente;
+import domain.Empleado;
+import java.util.Date;
+
+/**
+ *
+ * @author Gastón
+ */
+public class TestHerencia {
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado("Gaston", 57000.0);
+        System.out.println("empleado1 = " + empleado1);
+        
+        Date fecha1 = new Date();
+        
+        Cliente ciente1 = new Cliente("Bety", 32, 'F', "9 de Julio 1413", fecha1, true);
+        System.out.println("ciente1 = " + ciente1);
+    }
+}
